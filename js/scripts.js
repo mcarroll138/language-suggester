476af45f2befb53b96e49996e69e44d75d9f6da0
MCarroll138 <Mcarroll138@gmail.com>
2023-06-02 15:10:35 -0700

    debug, wip.

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,25 @@ function hideResults() {
 
 window.onload = function () {
   hideResults();
-  document.querySelector("form").onsubmit = function (event) {
+  let form = document.querySelector("form");
+  form.onsubmit = function (event) {
     event.preventDefault();
     hideResults;
   }
-};
+  function inputTotal() {
+    let colorInput = document.getElementById("color");
+    let foodInput = document.getElementById("food");
+    let animalInput = document.getElementById("animal");
+    let locationInput = document.getElementById("location");
+    let plutoInput = document.getElementById("pluto");
+
+    let selectedValueColor = colorInput.options[colorInput.selectedIndex].value;
+    let selectedValueFood = foodInput.options[foodInput.selectedIndex].value;
+    let selectedValueAnimal = animalInput.options[animalInput.selectedIndex].value;
+    let selectedValueLocation = locationInput.options[locationInput.selectedIndex].value;
+    let selectedValuePluto = plutoInput.options[plutoInput.selectedIndex].value;
+
+    let total = parseInt(selectedValueColor) + parseInt(selectedValueFood) + parseInt(selectedValueAnimal + parseInt(selectedValueLocation) + parseInt(selectedValuePluto));
+    console.log(total);
+  }
 };
