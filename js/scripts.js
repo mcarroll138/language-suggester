257a2e9488f17b573b22a0aa1f3e53a8d2978636
MCarroll138 <Mcarroll138@gmail.com>
2023-06-02 10:55:50 -0700

    change questions to options in html file.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function hideResults() {
+  document.getElementById("python").setAttribute("class", "hidden");
+  document.getElementById("ruby").setAttribute("class", "hidden");
+  document.getElementById("cSharp").setAttribute("class", "hidden");
+  document.getElementById("java").setAttribute("class", "hidden");
+}
+
+window.onload = function () {
+  document.querySelector("form").onsubmit = function (event) {
+    hideResults();
+    event.preventDefault();
+  }
+}
