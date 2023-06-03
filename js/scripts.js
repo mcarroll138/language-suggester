96f126cea110b13ae524cd16cec70de0aa6f2a71
MCarroll138 <Mcarroll138@gmail.com>
2023-06-02 17:17:36 -0700

    continue debug and test wip.

--- js/scripts.js
+++ js/scripts.js
@@ -18,6 +18,8 @@ window.onload = function () {
   });
 
   function inputTotal() {
+    let nameInput = document.getElementById("name");
+    let userName = nameInput.value;
     let colorInput = document.getElementById("color");
     let foodInput = document.getElementById("food");
     let animalInput = document.getElementById("animal");
@@ -40,12 +42,16 @@ window.onload = function () {
 
     if (total >= 30) {
       document.getElementById("python").classList.remove("hidden");
+      document.getElementById("python").innerHTML = "<h1>Great news " + userName + "! Pluto will always be a planet in my eyes too. I think Python would be a great fit for you!</h1>";
     } else if (total < 30) {
       document.getElementById("ruby").classList.remove("hidden");
+      document.getElementById("ruby").innerHTML = "<h1>Great news " + userName + "! I think Ruby is right up your alley!</h1>";
     } else if (total < 20) {
       document.getElementById("cSharp").classList.remove("hidden");
+      document.getElementById("cSharp").innerHTML = "<h1>Great news " + userName + "! For C#, first you got to be sharp and I think you got what it takes!</h1>";
     } else {
       document.getElementById("java").classList.remove("hidden");
+      document.getElementById("java").innerHTML = "<h1>Great news " + userName + "! I see heartache and pain in your future, followed by reward. I think JavaScript is the right path for you.</h1>";
     }
   }
 };
